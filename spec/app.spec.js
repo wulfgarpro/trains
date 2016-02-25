4e821377fecfeb4e4752805da81663437858dc0f
James Fraser <wulfgar.pro@gmail.com>
2016-02-25 12:15:46 +1100

    started ruminating functionality with tdd

---
+++ spec/app.spec.js
@@ -0,0 +1,23 @@
+describe('App', function(done) {
+    xit('Reads in input file', function(done) {
+
+    });
+    xit('Creates a valid diagraph from input file', function(done) {
+
+    });
+    xit('Calculates distance of a defined route', function(done) {
+
+    });
+    xit('Calculates number of possible trips with max stops', function(done) {
+
+    });
+    xit('Calculates number of possible trips with exact stops', function(done) {
+
+    });
+    xit('Calculates shortest possible route', function(done) {
+
+    });
+    xit('Calculates number of unique routes with distance relationship', function(done) {
+
+    });
+});
