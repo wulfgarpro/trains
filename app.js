0f815ccaa8aabd8e44c4dc075243331ee17e500a
James Fraser <wulfgar.pro@gmail.com>
2016-02-25 15:43:55 +1100

    added stubs to main app

--- app.js
+++ app.js
@@ -1,28 +1,61 @@
 /*
-The MIT License (MIT)
+    The MIT License (MIT)
 
-Copyright (c) 2016 James Fraser
+    Copyright (c) 2016 James Fraser
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
 
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
 */
 
+/*
+*/
+var Digraph = require('./lib/digraph');
+
+/*
+*/
 var App = function() {
-    console.log("Welcome to trains!");
-}
-var app = new App();
+    console.log('--------------------')
+    console.log('"Welcome to Trains!"');
+    console.log('--------------------')
+};
+
+/*
+*/
+App.prototype.calcDistance = function(path) {
+    return '';
+};
+
+/*
+*/
+App.prototype.calcNumberOfPossibleTrips = function(path, stops) {
+    return '';
+};
+
+/*
+*/
+App.prototype.calcShortestRoute = function(path) {
+    return '';
+};
+
+/*
+*/
+App.prototype.calcRouteCount = function(path, distance) {
+    return '';
+};
+
+module.exports = App;
